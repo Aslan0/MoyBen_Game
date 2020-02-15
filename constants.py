488ddeafdc4976cf24e9c0fa3b89c289a00ee6c7
Aslan0 <hoochihoo@googlemail.com>
2020-02-15 11:44:13 +0100

    Initial commit

---
+++ constants.py
@@ -0,0 +1,20 @@
+"""
+Global constants
+"""
+
+# Colors
+BLACK    = (   0,   0,   0) 
+WHITE    = ( 255, 255, 255) 
+BLUE     = (   0,   0, 255)
+
+# Screen dimensions
+SCREEN_WIDTH  = 800
+SCREEN_HEIGHT = 600
+
+import gtk
+
+width = gtk.gdk.screen_width()
+height = gtk.gdk.screen_height()
+
+# SCREEN_WIDTH  = width
+# SCREEN_HEIGHT = height
